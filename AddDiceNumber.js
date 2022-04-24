702611bd5a9eb1ca12a0c909c0a2f0b69c743f48
Siddhant <santoshshrivastava39@gmail.com>
2022-04-24 21:36:23 +0530

    Add two dice numbers

---
+++ AddDiceNumber.js
@@ -0,0 +1,13 @@
+//UC-3 Add two Random Dice Number..
+ 
+let dicenumber1 =Math.floor(Math.random() *6)+1;
+let dicenumber2 =Math.floor(Math.random() *6)+1;
+
+console.log("Number on Dice1 :" +dicenumber1);
+console.log("Number on Dice2 :" +dicenumber2);
+
+let result = dicenumber1+dicenumber2;
+console.log("Total of Two Dice Number is :" + result);
+
+
+
