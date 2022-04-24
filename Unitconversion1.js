69e16fccc6abe9f3fa3205bf5d4b3d33ac1f6bf3
Siddhant <santoshshrivastava39@gmail.com>
2022-04-24 23:45:13 +0530

    Unit Conversions

---
+++ Unitconversion1.js
@@ -0,0 +1,16 @@
+
+
+    //a- Inch to Ft
+   let a=42;
+   let result1=a/12;
+   console.log("42 in = "+result1+" ft");
+   
+
+   //b- Rectangular Plot of 60 feet x 40 feet in meters
+   let AreaInFeet=60*40;
+   let result2=AreaInFeet/10.764;
+   console.log("Plot area in meter : "+result2);
+    
+   //c- Calculate area of 25 such plots in acres
+   let result3=AreaInFeet*25/43560;
+    console.log("25 plots area in acres is : "+result3);
