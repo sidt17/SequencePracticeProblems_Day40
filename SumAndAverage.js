a708fd743ad41e3431d4b8bfc8af2766b8e7ba4a
Siddhant <santoshshrivastava39@gmail.com>
2022-04-24 22:17:06 +0530

    Find the sum and average

---
+++ SumAndAverage.js
@@ -0,0 +1,13 @@
+
+
+let totalrandomnumbers=5;
+let TotalSum=0;
+for(i=0;i<totalrandomnumbers;i++)
+{
+ let twodigitnumber = Math.floor(Math.random() *100);
+
+ TotalSum =TotalSum+twodigitnumber;
+}
+let average = TotalSum/5;
+console.log("The sum of 5 random two digit number is :" + TotalSum);
+console.log("The average of 5 random two digit number is :" + average);
