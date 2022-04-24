2e67abb1e882d97a92a530b75c105f0737693db9
Siddhant <santoshshrivastava39@gmail.com>
2022-04-24 21:06:55 +0530

    Use random function to get single digit

---
+++ SingleDigit.js
@@ -0,0 +1,6 @@
+ let a= Math.floor(Math.random() * 10);
+ //console.log("The single digit number is :" + a);
+
+
+ let dicenumber =Math.floor(Math.random() *6)+1;
+ console.log("Dice Number is :" + dicenumber);
